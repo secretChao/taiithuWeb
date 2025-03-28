9c7d46876cca8e9132462b6072b84c7fae34c070
SecretChao <secret0472@gmail.com>
2025-03-28 15:45:07 +0800

    fix link

--- src/main.js
+++ src/main.js
@@ -4,3 +4,17 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".index").forEach(a => {
+    console.log(a.href)
+    a.href = import.meta.env.BASE_URL + a.getAttribute("prop").substring(1);
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   document.getElementById("about-link").href = import.meta.env.BASE_URL + "about.html";
+  
+// });
