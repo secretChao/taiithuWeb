73042da186affa68504b0d3eb823aa202faf9dd2
SecretChao <secret0472@gmail.com>
2025-03-28 14:24:54 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
